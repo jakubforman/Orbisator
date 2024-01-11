aab525959beb379b66c643f4e2a282c0f2533c40
jakubforman <me@jakubforman.eu>
2024-01-11 15:54:21 +0100

    Implement calculator functionality and refactor code
    In this commit, methods for a simulated calculator are introduced. Click handlers are added to HTML elements in home.page.html to perform actions such as addition and sign toggling. The home.page.ts methods handle the logic behind these actions. Code is also refactored to include detailed comments and more streamlined construction.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -10,15 +10,89 @@ export class HomePage {
   /**
    * Current displayed value
    */
-  currentValue = "0";
+  currentValue: string = "0";
 
-  constructor() {}
+  /**
+   * Temporary value of currentValue
+   * @private
+   */
+  private tmpValue: string = '';
 
+  /**
+   * Temporary action of action button
+   *
+   * TODO: přepracovat string na type nebo enum
+   * @private
+   */
+  private tmpAction: string = '';
+
+  constructor() {
+  }
+
+  /**
+   * Clears the current value.
+   *
+   * @returns {void}
+   */
   clear() {
     this.currentValue = "0";
   }
 
+  /**
+   * Adds a number to the currentValue.
+   *
+   * @param {number} number - The number to be added.
+   */
   add(number: number) {
-    this.currentValue = `${this.currentValue}${number}`;
+    if (this.currentValue === '0') {
+      this.currentValue = `${number}`;
+    } else {
+      this.currentValue = `${this.currentValue}${number}`;
+    }
+  }
+
+  /**
+   * Toggles the sign of the current value.
+   *
+   * @return {void}
+   */
+  toggleSign() {
+    if (!isNaN(Number(this.currentValue))) {
+      this.currentValue = (Number(this.currentValue) * -1).toString()
+    }
+  }
+
+  /**
+   * Adds the current value to the temporary value and updates the current value.
+   *
+   * @return {void}
+   */
+  plus() {
+    this.tmpAction = 'plus';
+
+    if (this.tmpValue) {
+      this.tmpValue =
+        (Number(this.tmpValue) + Number(this.currentValue)).toString();
+
+      this.currentValue = this.tmpValue;
+    } else {
+      this.tmpValue = this.currentValue;
+    }
+    this.currentValue = "0";
+  }
+
+  /**
+   * Sets the currentValue to the tmpValue and clears the tmpValue.
+   * If tmpAction is 'plus', calls the 'plus' method.
+   *
+   * @returns {void} - No return value.
+   */
+  equal() {
+    if (this.tmpAction === 'plus') {
+      this.plus();
+    }
+
+    this.currentValue = this.tmpValue;
+    this.tmpValue = "";
   }
 }
